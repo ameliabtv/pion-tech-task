c6ccaafe2cdb77444130920430b567f408ee8944
ameliabtv <amelia.betivu@gmail.com>
2026-03-08 20:51:53 +0200

    Update int_subscriptions_enriched.sql

--- models/intermediate/int_subscriptions_enriched.sql
+++ models/intermediate/int_subscriptions_enriched.sql
@@ -1,1 +1,16 @@
+with subscriptions as (
+  select * from {{ ref('stg_subscriptions') }}
+), 
 
+plan as (
+  select * from {{ ref('stg_plans') }}
+  
+),
+
+joined as (
+  select s.subscription_id, s.customer_id, s.plan_id, s.status_code, s.start_date, s.end_date, p.plan_name, p.monthly_cost
+  from subscriptions s
+  left join plan p on s.plan_id = p.plan_id
+)
+
+select * from joined
