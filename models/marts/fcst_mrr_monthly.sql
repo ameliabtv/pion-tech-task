4a6a3df761d6faedf1d0d9465b6e8d6d05290290
ameliabtv <amelia.betivu@gmail.com>
2026-03-08 22:07:56 +0200

    Update fcst_mrr_monthly.sql

--- models/marts/fcst_mrr_monthly.sql
+++ models/marts/fcst_mrr_monthly.sql
@@ -1,1 +1,16 @@
+with months as (
+select * from {{ ref('int_subscription_months') }}
+),
 
+mrr as (
+select
+customer_id,
+revenue_month,
+sum(monthly_cost) as mrr
+from months
+group by
+customer_id,
+revenue_month
+)
+
+select * from mrr
